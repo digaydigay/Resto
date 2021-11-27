29819cbe2f080be8b29098acecf1d45c9312f645
Jonathan Digay <jonthan.digay1@gmail.com>
2021-11-27 09:18:36 +0800

    init

--- components/Auth/signup.js
+++ components/Auth/signup.js
@@ -31,6 +31,27 @@ export default function Signup() {
           <label>Confirm Password</label>
           <input type="text" placeholder="confirm password..." />
         </div>
+        <div className="input-group">
+          <label>Name</label>
+          <input type="text" placeholder="Complete Name..." />
+        </div>
+        <div className="input-group">
+          <label>Email</label>
+          <input type="text" placeholder="email address..." />
+        </div>
+        <div className="input-group">
+          <label>Address</label>
+          <input type="text" placeholder="Your location..." />
+        </div>
+
+        <div className="input-group">
+          <label>Password</label>
+          <input type="text" placeholder="account password..." />
+        </div>
+        <div className="input-group">
+          <label>Confirm Password</label>
+          <input type="text" placeholder="confirm password..." />
+        </div>
         <div className="actions">
           <button type="submit">Sign up</button>
           <button type="button" onClick={() => dispatch(cancel())}>
